09155da4a37f6ff7486e54f304826179ebe36321
clime <clime@redhat.com>
2016-03-22 08:09:52 +0100

    updates (mighty)

--- scripts/berestart.sh
+++ scripts/berestart.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sudo systemctl restart copr-backend-jobgrab.service copr-backend-log.service copr-backend.service copr-backend-vmm.service redis.service
+sudo journalctl -x -n 20 -u copr-backend
+sleep 1
+sudo systemctl list-units | grep copr
